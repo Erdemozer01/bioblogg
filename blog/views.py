accbe2cc2091e0fa24626dd8aa8b604cad860806
Erdemozer1 <20219248004@cumhuriyet.edu.tr>
2022-12-27 01:56:29 +0300

    files updated

--- blog/views.py
+++ blog/views.py
@@ -329,7 +329,7 @@ class CommentUpdate(generic.UpdateView):
     def get_success_url(self):
         comment = self.get_object()
         messages.success(self.request, "Yorumunuz başarılı bir şekilde güncellendi..")
-        return redirect('blog:post_detail', args=(
+        return reverse('blog:post_detail', args=(
             comment.post.category.title, comment.post.slug, comment.post.pk, comment.post.author, comment.post.author.id,
             comment.post.created.date()
         ))
