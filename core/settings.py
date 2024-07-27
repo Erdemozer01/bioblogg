29630dac9bd76e0705af08a970d4ece6352ecddb
Erdemozer01 <ozer246@gmail.com>
2024-07-27 20:18:59 +0300

    güncellendi

--- core/settings.py
+++ core/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static/"
+    BASE_DIR / "staticfiles/"
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles/"
+STATIC_ROOT = BASE_DIR / "static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
