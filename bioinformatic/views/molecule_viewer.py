4e6aecf28d09e57dc1863259facaa4175e441cf1
Erdemozer01 <ozer246@gmail.com>
2024-07-30 04:48:36 +0300

    güncellendi

--- bioinformatic/views/molecule_viewer.py
+++ bioinformatic/views/molecule_viewer.py
@@ -659,7 +659,7 @@ def multi_molecule_view(request):
 
     form = MultiMoleculeViewForm(request.POST or None, request.FILES or None)
 
-    data_path = os.path.join(BASE_DIR, "media", "laboratory", f"{request.user}\\")
+    data_path = os.path.join(BASE_DIR, "media", "laboratory", f"{request.user}\\").replace("\\", "/")
 
     if request.method == "POST":
 
@@ -712,10 +712,6 @@ def multi_molecule_view(request):
                 {"label": "SİYAH", "value": "black"},
                 {"label": "BEYAZ", "value": "white"},
             ]
-
-            for j in obj.records_files.filter(records__user=request.user):
-                print(j.file.path)
-                print(j.file.name.split('/')[-1])
 
             app.layout = dbc.Card(
                 [
