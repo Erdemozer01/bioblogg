fa80d4536652a761dbbb781c2d895fde5b51bc10
Erdemozer01 <ozer246@gmail.com>
2024-08-31 15:46:26 +0300

    güncellendi

--- bioinformatic/views/molecule_viewer.py
+++ bioinformatic/views/molecule_viewer.py
@@ -695,7 +695,7 @@ def multi_molecule_view(request):
 
     form = MultiMoleculeViewForm(request.POST or None, request.FILES or None)
 
-    data_path = os.path.join(BASE_DIR, "media", "laboratory", f"{request.user}/")
+    data_path = os.path.join(BASE_DIR, "media", "laboratory")
 
     if request.method == "POST":
 
